a75bfa54707de04b6a6c5e5bf25ba314170dbb50
WebersonRodrigues <weberson@deeprocket.com.br>
2023-10-27 11:48:05 -0300

    Criado o html da tela de Cadastro

--- crudBasico/src/views/Cadastrar.vue
+++ crudBasico/src/views/Cadastrar.vue
@@ -1,5 +1,53 @@
 <template>
-    <div>Cadastrar</div>
+    <div>
+        <div class="input-container">
+          <label for="titulo">Titulo: </label>  
+          <input type="text" id="titulo" v-model="titulo" placeholder=" Digite o titulo" >
+        </div>
+       <br>
+
+        <div class="input-container">
+          <label for="veículo">  Veículo : </label>  
+          <input type="text" id="veículo" v-model="veículo" placeholder=" Digite o  Veículo ">
+        </div>
+        <br>
+      
+        <div class="input-container">
+          <label for="url">  Url da imagem : </label>  
+          <input type="text" id="url" v-model="url" placeholder=" ">
+        </div>
+        <br>
+
+        <div class="input-container">
+          <label for="descricao">  Descrição : </label>  
+          <input type="text" id="descricao" v-model="descricao" placeholder=" ">
+        </div>
+        <br>
+        
+        <div class="input-container">
+          <label for="email"> Email : </label>  
+          <input type="text" id="email" v-model="email" placeholder=" Digite seu e-mail">
+        </div>
+        <br>
+
+        <div class="input-container">
+          <label for="nome"> Nome : </label>  
+          <input type="text" id="nome" v-model="nome" placeholder=" Digite seu nome">
+        </div>
+        <br>
+        
+        <div class="input-container">
+          <label for="altura"> Altura : </label>  
+          <input type="text" id="email" v-model="email" placeholder=" Digite a altura">
+        </div>
+        <br>
+    
+        <div class="input-container">
+          <label for="largura"> Largura : </label>  
+          <input type="text" id="largura" v-model="largura" placeholder=" Digite a largura">
+        </div>
+        <br>
+    </div>
 </template>
 
 <script>
@@ -10,37 +58,10 @@ export default {
 </script>
 
 <style>
-*{
-    background-color: transparent;  
-}
-.navbar {
-    display: flex;
-    width: 100%;
-    height: 70px;
-    padding: 5px;
-    background-color: #006D77 ;
-    justify-content: space-between;
+.input-container {
+    margin-top: 10px;
+    flex-direction: column;
     align-items: center;
 
 }
-.img-logo-alterdata {
-    width: 60px;
-}
-.logo-alterdata {
-    width: 10%;
-    height: auto;
-    display: flex;
-    background-color: transparent;
-}
-.menu {
-    width: 100%;
-    height: auto;
-    display: flex;
-}
-.font-menu{
-    text-decoration: none;
-    color: #fff;
-    font-size: 20px;
-}
-
 </style>
