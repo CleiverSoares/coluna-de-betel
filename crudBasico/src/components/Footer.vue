85b6d115970493863ea5928f4dcbd88b856f8dd2
chiappetta-rafael <149080317+chiappetta-rafael@users.noreply.github.com>
2023-10-30 09:47:17 -0300

    Criação do Footer

---
+++ crudBasico/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+            <div>
+                <p>Direitos reservados a Cleiver</p>
+            </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer",
+    };
+</script>
+
+<style scoped>
+    footer {
+        display: flex;
+        align-items: center;
+        height: 50px;
+        background-color: #83c5be;
+        margin-top: 8%;
+    }
+</style>
