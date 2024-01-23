ab52a92ed649229503a0482c947a65038c8bb058
CleiverSoares <cleiversoares2@gmail.com>
2024-01-23 01:43:39 -0300

    teste slider

--- coluna-de-betel/src/views/Home.vue
+++ coluna-de-betel/src/views/Home.vue
@@ -4,13 +4,104 @@
       <!-- Conteúdo da imagem de fundo -->
     </div>
     <div class="div-video" v-else>
-      <video controls autoplay muted>
-        <source src="../assets/img/video.mp4" type="video/mp4">
-      </video>
+
+
+
+
+
+
+      <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="center: true">
+
+        <ul class="uk-slider-items uk-grid uk-grid-match" uk-height-viewport="offset-top: true; offset-bottom: 30">
+          <li class="uk-width-1-1">
+            <div class="uk-cover-container">
+              <div class="video-container">
+                <video controls autoplay muted class="uk-width-1-1 uk-height-1-1">
+                  <source src="../assets/img/video.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="uk-position-center uk-panel">
+                <h1>1</h1>
+              </div>
+            </div>
+          </li>
+          <li class="uk-width-1-1">
+            <div class="uk-cover-container">
+              <div class="video-container">
+                <video controls autoplay muted>
+                  <source src="../assets/img/video.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="uk-position-center uk-panel">
+                <h1>2</h1>
+              </div>
+            </div>
+          </li>
+          <li class="uk-width-1-1">
+            <div class="uk-cover-container">
+              <div class="video-container">
+                <video controls autoplay muted>
+                  <source src="../assets/img/video.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="uk-position-center uk-panel">
+                <h1>3</h1>
+              </div>
+            </div>
+          </li>
+          <li class="uk-width-1-1">
+            <div class="uk-cover-container">
+              <div class="video-container">
+                <video controls autoplay muted>
+                  <source src="../assets/img/video.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="uk-position-center uk-panel">
+                <h1>4</h1>
+              </div>
+            </div>
+          </li>
+          <li class="uk-width-1-1">
+            <div class="uk-cover-container">
+              <div class="video-container">
+                <video controls autoplay muted>
+                  <source src="../assets/img/video.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="uk-position-center uk-panel">
+                <h1>5</h1>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href uk-slidenav-previous
+          uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next
+          uk-slider-item="next"></a>
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="video-container">
+        <video controls autoplay muted>
+          <source src="../assets/img/video.mp4" type="video/mp4">
+        </video>
+      </div>
       <div class="overlay"></div>
       <div class="overlay-text">
-        <p>Coluna de Betel
-        </p>
+        <p>Coluna de Betel</p>
       </div>
     </div>
   </div>
@@ -48,6 +139,7 @@ export default {
 .home-container {
   width: 100%;
   height: 100%;
+  justify-content: center;
 }
 
 .div-imagem {
@@ -64,7 +156,18 @@ export default {
   position: relative;
   height: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: center !important;
+  padding: 10px 0px;
+}
+
+.video-container {
+  overflow: hidden;
+  border-radius: 10px;
+}
+
+video {
+  width: 100%;
+  height: 100%;
 }
 
 .overlay {
@@ -75,6 +178,7 @@ export default {
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
+  pointer-events: none;
 }
 
 .overlay-text {
@@ -87,9 +191,4 @@ export default {
   color: rgba(255, 255, 255, 0.7);
   font-size: 24px;
   font-weight: bold;
-}
-
-video {
-  width: 100%;
-  height: 100%;
 }</style>
